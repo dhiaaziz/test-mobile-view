974d0daa32c22ee1c379483b86f6984e92e8141a
Dhia Aziz <azizwkwkwk@gmail.com>
2018-05-02 16:47:28 +0700

    coba cobe2345

--- assets/js/login.js
+++ assets/js/login.js
@@ -9,34 +9,34 @@ setTimeout(function () {
     viewport.setAttribute("content", "height=" + viewheight + "px, width=" + viewwidth + "px, initial-scale=1.0");
 }, 300);
 
-//prevent caret to overflow
-// var obj = document.getElementById('partitioned');
-// obj.addEventListener("keydown", stopCarret);
-// obj.addEventListener("keyup", stopCarret);
+// prevent caret to overflow
+var obj = document.getElementById('partitioned');
+obj.addEventListener("keydown", stopCarret);
+obj.addEventListener("keyup", stopCarret);
 
-// function stopCarret() {
-// if (obj.value.length > 3){
-//   setCaretPosition(obj, 3);
-// }
-// }
-//
-// function setCaretPosition(elem, caretPos) {
-//   if(elem != null) {
-//       if(elem.createTextRange) {
-//           var range = elem.createTextRange();
-//           range.move('character', caretPos);
-//           range.select();
-//       }
-//       else {
-//           if(elem.selectionStart) {
-//               elem.focus();
-//               elem.setSelectionRange(caretPos, caretPos);
-//           }
-//           else
-//               elem.focus();
-//       }
-//   }
-// }
+function stopCarret() {
+if (obj.value.length > 3){
+  setCaretPosition(obj, 3);
+}
+}
+
+function setCaretPosition(elem, caretPos) {
+  if(elem != null) {
+      if(elem.createTextRange) {
+          var range = elem.createTextRange();
+          range.move('character', caretPos);
+          range.select();
+      }
+      else {
+          if(elem.selectionStart) {
+              elem.focus();
+              elem.setSelectionRange(caretPos, caretPos);
+          }
+          else
+              elem.focus();
+      }
+  }
+}
 
 
 $(document).ready(function(){
